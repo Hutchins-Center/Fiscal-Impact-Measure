37b142d075ec7b6926b3d44b530e5326efdcd86d
Manu Alcala Kovalski <malcalakovalski@brookings.edu>
2020-09-29 02:44:48 +1100

    load ggplot

--- code/fim_datapull.R
+++ code/fim_datapull.R
@@ -17,7 +17,7 @@ mPackages <- installed.packages()
 stInstalled <- rownames( mPackages )
 # Isolate thep package names
 stRequired <- c('tidyverse','stringr','reshape2', 'zoo', 'quantmod', 'rmarkdown', 'TTR',
-   'data.table', 'lubridate', 'Hmisc')
+   'data.table', 'lubridate', 'Hmisc', 'ggplot2')
 #  The required packages
 
 for ( stName in stRequired ){
