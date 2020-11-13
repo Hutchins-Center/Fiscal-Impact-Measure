98752bcd259d4d54be16082638ea9c5c567be5c4
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2020-11-13 17:54:58 -0500

    checked consistency with federal bea nipas

--- development/features/nipa-consistent-FIM/testing/compare-to-nipas.R
+++ development/features/nipa-consistent-FIM/testing/compare-to-nipas.R
@@ -5,9 +5,10 @@ nipa_path <- "development/features/nipa-consistent-FIM"
 nipa_results_path <- paste0(nipa_path, '/results/', thismonth, '/data')
 
 
+# State ---------------------------------------------------------------------------------------
+
 bea_state <- read_xlsx(here(nipa_path, 'bea-tables', 'state-receipts-expenditures.xlsx')) %>%
   mutate(date = as.Date(date))
-
 
 bea_start <- min(bea_state$date)
 bea_end <- max(bea_state$date)
@@ -29,4 +30,26 @@ summary(
                   bea_state,
                   tol.num.val = 0.1)
   )
+
+
+# Federal -------------------------------------------------------------------------------------
+
+fim_nipa_federal <-
+  fim_nipa %>% 
+  select(date, contains('federal'),
+         -contains(c('cont','net', 'add', 'pi'))
+  )
+bea_federal <- read_xlsx(here(nipa_path, 'bea-tables', 'federal-receipts-expenditures.xlsx')) %>%
+  mutate(date = as.Date(date))
+
+
+
+summary(
+  comparedf(
+    fim_nipa_federal %>%
+      mutate(federal_social_benefits = federal_social_benefits + federal_health_outlays),
+    bea_federal,
+    tol.num.val = 0.1)
+)
+
 
