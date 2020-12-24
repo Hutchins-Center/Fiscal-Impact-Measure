50efe04099fa9dae99d8f7ff1b05c1e240c2955b
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2020-12-24 15:29:58 -0500

    fim update 12/24/2020
    Incorporates new stimulus bill

--- reports/stimulus/stimulus-changes.R
+++ reports/stimulus/stimulus-changes.R
@@ -25,14 +25,17 @@ write_xlsx(no_stimulus, glue('{update_path}/fim_nostimulus.xlsx'))
 stimulus <-
   read_excel(glue('{update_path}/fim.xlsx'), na = "NA") %>%
   filter(date > '2016-12-31') %>%
-  select(date, ends_with('net_taxes'), ends_with('xmpc')) %>%
+  select(date, ends_with('net'), ends_with('xmpc')) %>%
   mutate(date = as.Date(date))
 
 no_stimulus <- 
   read_excel(glue('{update_path}/no-stimulus/fim_nostimulus.xlsx'), na = "NA") %>%
   filter(date > '2016-12-31') %>%
-  select(date, ends_with('net_taxes'), ends_with('xmpc')) %>%
+  select(date, ends_with('net'), ends_with('xmpc')) %>%
   mutate(date = as.Date(date))
 
-write_xlsx(stimulus, glue('{update_path}/fim_stimulus_net_xmpc.xlsx'))
-write_xlsx(no_stimulus, glue('{update_path}/fim_nostimulus_net_xmpc.xlsx'))
+write_xlsx(stimulus, glue('{update_path}/fim_stimulus_netc.xlsx'))
+write_xlsx(no_stimulus, glue('{update_path}/fim_nostimulus_net.xlsx'))
+
+purchases_diff <-
+  stimulus$federal_nom - no_stimulus$federal_nom
