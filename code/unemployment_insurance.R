c75c07ec34316419e5224911a4003809abfb639b
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-02-23 17:55:45 -0500

    Revert "Merge branch 'budget_projections'"
    This reverts commit 1cde3e7a4ccb72f21b5faca56dac0221d1fd1f8e, reversing
    changes made to 799387fbf593ec436a673df2ede6bf302785a051.

--- code/unemployment_insurance.R
+++ code/unemployment_insurance.R
@@ -1,84 +1,73 @@
-source("R/packages.R")
-library("lubridate")
-library("janitor")
-library("tsibble")
-library("dplyr")
+source('R/packages.R')
+library('lubridate')
+library('janitor')
+library('tsibble')
+library('dplyr')
 # Functions -----------------------------------------------------------------------------------
-get_cbo_ui <- function() {
-  read_xlsx("data/pandemic-legislation/pandemic-legislation.xlsx",
-    sheet = "annual"
-  ) %>%
+get_cbo_ui <- function(){
+  read_xlsx('data/pandemic-legislation/pandemic-legislation.xlsx',
+            sheet = 'annual') %>%
     select(-1) %>%
     pivot_longer(where(is.numeric)) %>%
     pivot_wider(names_from = date, values_from = value) %>%
     clean_names() %>%
     select(-c(economic_impact_payments, coronavirus_relief_fund, na)) %>%
-    rename(
-      year = name,
-      total_ui = total_unemployment_insurance,
-      peuc = pandemic_emergency_unemployment_compensation,
-      pua = pandemic_unemployment_assistance,
-      puc = pandemic_unemployment_compensation_payments
-    )
-  # annual_to_quarter(year) %>%
-  # mutate(total_ui = total_ui + peuc,
-  #        peuc = peuc * 2) %>%
-  # mutate(fy = fiscal_year(yq)) %>%
-  # index_by(fy)
+    rename(year = name, 
+           total_ui = total_unemployment_insurance,
+           peuc = pandemic_emergency_unemployment_compensation,
+           pua = pandemic_unemployment_assistance, 
+           puc = pandemic_unemployment_compensation_payments) 
+    # annual_to_quarter(year) %>%
+    # mutate(total_ui = total_ui + peuc,
+    #        peuc = peuc * 2) %>%
+    # mutate(fy = fiscal_year(yq)) %>%
+    # index_by(fy)
 }
-legislation_weights <- function(df) {
+legislation_weights <- function(df){
   df %>%
-    mutate(
-      peuc = peuc * peuc_weights(),
-      pua = pua * pua_weights(),
-      puc = puc * puc_weights(),
-      other = other * other_weights()
+    mutate(peuc = peuc * peuc_weights(),
+           pua = pua * pua_weights(),
+           puc = puc * puc_weights(),
+           other = other * other_weights()
     )
 }
-decompose_totals <- function(df) {
+decompose_totals <- function(df){
   df %>%
-    mutate(
-      ui_legislation = 4 * (peuc + pua + puc + other),
-      ui_as = total_ui - ui_legislation
-    )
+    mutate(ui_legislation = 4 * (peuc + pua + puc + other),
+           ui_as = total_ui - ui_legislation)
 }
-ui_allocation <- function(df) {
+ui_allocation <- function(df){
   df %>%
-    rename(
-      federal_ui = ui_legislation,
-      state_ui = ui_as
-    )
+    rename(federal_ui = ui_legislation,
+           state_ui = ui_as)
 }
-clean_cbo_ui <- function(df) {
+clean_cbo_ui <- function(df){
   df %>%
     ungroup() %>%
     index_by(yq) %>%
-    mutate(yq = yearquarter(date, fiscal_start = 1))
+    mutate(yq = yearquarter(date, fiscal_start = 1)) 
 }
 
-get_monthly_ui <- function() {
-  read_xlsx("data/pandemic-legislation/pandemic-legislation.xlsx",
-    sheet = "monthly"
-  ) %>%
+get_monthly_ui <- function(){
+  read_xlsx('data/pandemic-legislation/pandemic-legislation.xlsx',
+            sheet = 'monthly') %>%
     pivot_longer(where(is.numeric)) %>%
-    pivot_wider(names_from = date, values_from = value) %>%
+    pivot_wider(names_from = date, values_from = value) %>% 
     rename(date = name) %>%
-    mutate(date = as_date(as.numeric(date), origin = "1899-12-30")) %>%
+    mutate(date = as_date(as.numeric(date), origin = '1899-12-30'))  %>%
     clean_names() %>%
-    rename(
-      peuc = pandemic_emergency_unemployment_compensation,
-      pua = pandemic_unemployment_assistance,
-      puc = pandemic_unemployment_compensation_payments,
-      wla = wages_lost_assistance_program
-    ) %>%
-    mutate(
-      peuc = 2 * peuc,
-      total_ui = unemployment_insurance + wla,
-      federal_ui = (peuc + pua + puc + wla),
-      state_ui = total_ui - federal_ui
-    )
+  rename(
+    peuc = pandemic_emergency_unemployment_compensation,
+    pua = pandemic_unemployment_assistance, 
+    puc = pandemic_unemployment_compensation_payments,
+    wla = wages_lost_assistance_program
+  ) %>%
+    mutate(peuc = 2 * peuc,
+           total_ui = unemployment_insurance + wla,
+           federal_ui = (peuc + pua + puc + wla),
+           state_ui = total_ui - federal_ui)
 }
-monthly_to_quarterly <- function(df) {
+monthly_to_quarterly <- function(df){
   df %>%
     mutate(yq = tsibble::yearquarter(date)) %>%
     as_tsibble(index = date) %>%
@@ -86,61 +75,55 @@ monthly_to_quarterly <- function(df) {
     index_by(yq) %>%
     mutate(
       across(
-        .cols = where(is.numeric),
+        .cols = where(is.numeric), 
         .fns = ~ mean(.x)
       )
     ) %>%
     distinct(yq, .keep_all = TRUE) %>%
     select(-date)
 }
-trim_quarterly <- function(df) {
+trim_quarterly <- function(df){
   df %>%
     select(yq, total_ui, federal_ui, state_ui)
 }
-get_quarterly_ui <- function() {
-  read_xlsx("data/pandemic-legislation/pandemic-legislation.xlsx",
-    sheet = "quarterly"
-  ) %>%
+get_quarterly_ui <- function(){
+  read_xlsx('data/pandemic-legislation/pandemic-legislation.xlsx',
+            sheet = 'quarterly') %>%
     pivot_longer(where(is.numeric)) %>%
-    pivot_wider(names_from = date, values_from = value) %>%
+    pivot_wider(names_from = date, values_from = value) %>% 
     rename(date = name) %>%
     mutate(yq = yearquarter(date)) %>%
     clean_names() %>%
-    rename(
-      peuc = pandemic_emergency_unemployment_compensation,
-      pua = pandemic_unemployment_assistance,
-      puc = pandemic_unemployment_compensation_payments,
-      lwa = lost_wages_supplemental_payments,
-      total_ui = unemployment_insurance
-    ) %>%
-    mutate(
-      peuc = peuc * 2,
-      total_ui = total_ui + lwa
-    )
+    rename(peuc = pandemic_emergency_unemployment_compensation,
+           pua = pandemic_unemployment_assistance,
+           puc = pandemic_unemployment_compensation_payments,
+           lwa = lost_wages_supplemental_payments,
+           total_ui = unemployment_insurance) %>%
+    mutate(peuc = peuc * 2,
+           total_ui = total_ui + lwa)
 }
-peuc_weights <- function() {
-  weights_2021 <- c(0.31, 0.42, 0.27, 0)
+peuc_weights <- function(){
+  weights_2021 <- c(0.31,	0.42,	0.27, 0)
   return(weights_2021)
 }
-pua_weights <- function() {
-  weights_2021 <- c(0.71, 0.29, 0, 0)
+pua_weights <- function(){
+  weights_2021 <- c(0.71, 0.29,	0, 0)
   return(weights_2021)
 }
-puc_weights <- function() {
+puc_weights <- function(){
   weights_2021 <- c(1, rep(0, 3))
   return(weights_2021)
 }
-other_weights <- function() {
+other_weights <- function(){
   weights <- c(rep(0.4, 2), 0.2, 0)
 }
-fill_quarter <- function(df) {
+fill_quarter <- function(df){
   length <- df %>%
-    count() %>%
-    pull()
+    count() %>% pull()
   missing_months <- 3 - length %% 3
   df %>%
-    mutate(date = yearmonth(date)) %>%
-    mutate(quarter = yearquarter(date)) %>%
+    mutate(date  = yearmonth(date)) %>%
+    mutate(quarter = yearquarter(date))  %>% 
     tsibble::as_tsibble(index = date) %>%
     tsibble::append_row(n = missing_months) %>%
     fill(quarter) %>%
@@ -149,24 +132,23 @@ fill_quarter <- function(df) {
 
 
 
-puc_end <- function(df) {
-  puc_expiration_date <- "2020-12-31"
+puc_end <- function(df){
+  puc_expiration_date <- '2020-12-31'
   df %>%
     mutate(pandemic_unemployment_compensation_payments = if_else(date > puc_expiration_date, 0,
-      pandemic_unemployment_compensation_payments
-    ))
+                                                                  pandemic_unemployment_compensation_payments))
 }
-annualize <- function(x) {
+annualize <- function(x){
   x * 4
 }
-deannualize <- function(x) {
+deannualize <- function(x){
   x / 4
 }
-automatic_stabilizers_weights <- function() {
+automatic_stabilizers_weights <- function(){
   weights_2021 <- c(rep(0.26, 2), rep(0.24, 2))
   weights_2022 <- c(0.34, rep(0.22, 3))
-  weights_2023 <- c(rep(0.25, 4))
-
+  weights_2023 <- c(rep(0.25,4))
+  
   return(c(weights_2021, weights_2022, weights_2023))
 }
 automatic_stabilizers_weights <- automatic_stabilizers_weights()
@@ -174,25 +156,22 @@ monthly_legislation <- get_monthly_ui()
 quarterly_legislation <- get_quarterly_ui()
 
 
-get_second_draw <- function() {
-  read_xlsx("data/pandemic-legislation/pandemic-legislation.xlsx",
-    sheet = "stimulus-second-draw-summary"
-  ) %>%
+get_second_draw  <- function(){
+  read_xlsx('data/pandemic-legislation/pandemic-legislation.xlsx',
+            sheet = 'stimulus-second-draw-summary') %>%
     clean_names() %>%
     pivot_wider(names_from = type, values_from = total_b) %>%
     clean_names() %>%
     mutate(
       across(
         where(is.numeric),
-        .fns = ~ annualize(.x)
+        .fns = ~annualize(.x)
       )
     ) %>%
-    mutate(
-      date = as_date("2021-03-31"),
-      yq = yearquarter(date)
-    )
+    mutate(date = as_date('2021-03-31'),
+           yq = yearquarter(date)) 
 }
-get_ui_second_draw <- function(df) {
+get_ui_second_draw <- function(df){
   df %>%
     rename(ui_second_draw = unemployment_benefits) %>%
     select(yq, ui_second_draw)
@@ -201,60 +180,51 @@ get_ui_second_draw <- function(df) {
 
 # Data ----------------------------------------------------------------------------------------
 cbo_ui_raw <- get_cbo_ui()
-# cbo_ui <-
-
-get_cbo_ui() %>%
-  mutate(
-    peuc = peuc * 2,
-    legislation = peuc + pua + puc + other,
-    non_legislation = total_ui - legislation
-  ) %>%
+#cbo_ui <- 
+  
+  get_cbo_ui() %>%
+  mutate(peuc = peuc * 2, 
+         legislation = peuc+pua+puc+other, 
+         non_legislation = total_ui - legislation) %>%
   annual_to_quarter(year) %>%
   mutate(fy = fiscal_year(yq)) %>%
   index_by(fy) %>%
   legislation_weights() %>%
-  mutate(
-    ui_legislation = 4 * (peuc + pua + puc + other),
-    ui_as = total_ui - ui_legislation
-  ) %>%
+  mutate(ui_legislation = 4 * (peuc + pua + puc + other),
+         ui_as = total_ui - ui_legislation) %>%
   ui_allocation() %>%
   clean_cbo_ui() %>%
   select(date, yq, total_ui, federal_ui, state_ui, peuc, pua, puc, other)
 
 monthly_ui <-
-  get_monthly_ui() %>%
-  monthly_to_quarterly() %>%
-  trim_quarterly()
+  get_monthly_ui() %>% 
+    monthly_to_quarterly() %>%
+    trim_quarterly()
 
-quarterly_ui <-
+ quarterly_ui <- 
   get_quarterly_ui() %>%
-  mutate(
-    federal_ui = (peuc + pua + puc + lwa),
-    state_ui = total_ui - federal_ui
-  ) %>%
-  select(yq, total_ui, federal_ui, state_ui)
+    mutate(federal_ui = (peuc + pua + puc + lwa),
+           state_ui = total_ui - federal_ui) %>%
+    select(yq, total_ui, federal_ui, state_ui)
 
-ui_second_draw <-
+ui_second_draw <-  
   get_second_draw() %>%
   get_ui_second_draw()
-
+  
 ui_override <-
   monthly_ui %>%
   full_join(cbo_ui) %>%
   full_join(ui_second_draw) %>%
-  mutate(
-    ui_second_draw = coalesce(ui_second_draw, 0),
-    federal_ui = federal_ui + ui_second_draw,
-    state_ui = if_else(yq >= yearquarter("2021 Q2"), 44, state_ui),
-    total_ui = federal_ui + state_ui
-  ) %>%
+  mutate(ui_second_draw = coalesce(ui_second_draw, 0),
+         federal_ui = federal_ui + ui_second_draw,
+         state_ui = if_else(yq >= yearquarter('2021 Q2'), 44, state_ui),
+         total_ui = federal_ui + state_ui) %>%
   distinct(yq, .keep_all = TRUE) %>%
   mutate(date = lubridate::date(yq) + months(3) - days(1)) %>%
-  rename(
-    total_ui_override = total_ui,
-    federal_ui_override = federal_ui,
-    state_ui_override = state_ui
-  )
+  rename(total_ui_override = total_ui,
+         federal_ui_override = federal_ui,
+         state_ui_override = state_ui)
+
 
 
 # Export to FIM -------------------------------------------------------------------------------
