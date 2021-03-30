cf59bd19c55bf54264f2b498ed4e6b51334156e4
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2021-03-30 15:09:59 -0400

    new mpc for ui

--- R/mpc.R
+++ R/mpc.R
@@ -16,22 +16,6 @@ mpc_rebate_checks <- function(x){
   
 }
 
-#' Marginal propensity to consume for Subsidies 
-#'
-#' @param df 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-# mpc_unemployment_insurance <- function(x){
-#   
-#   mpc <- 0.9 
-#   weights <- c(rep(0.253, 2), rep(0.161, 2), 0.075, 0.05, 0.025, 0.022)
-#   mpc * roll::roll_sum(x, width = length(weights), weights = rev(weights), 
-#                        online = FALSE)
-#     
-# }
 
 #' Marginal Propensity to Consume Benefits in American Rescue Plan for Vulnerable Households
 #' Includes UI, Snap, Housing assistance, TANIF, WIC, Cobra Subsidies, etc
